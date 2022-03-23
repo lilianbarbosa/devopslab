a9552852afb8d42e34dfe48e7019f61bfb8fca3b
lilianbarbosa <86434245+lilianbarbosa@users.noreply.github.com>
2022-03-23 16:36:09 -0500

    Enabling tests

--- test.py
+++ test.py
@@ -17,3 +17,6 @@ class Test(unittest.TestCase):
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Hello World - Lilian")
+
+if __name__ == '__main__':
+    unittest.main()
